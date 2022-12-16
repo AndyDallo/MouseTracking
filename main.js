68c9fc9431bce2fe0edffdc73e7f1900322037d7
AndyDallo <dallo.inbox@gmail.com>
2022-12-16 18:05:11 +0100

    1st commit

---
+++ main.js
@@ -0,0 +1,81 @@
+function main() {
+    function lerp (start, end, t){
+        return (1 - t) * start + t * end;
+    }
+    
+    function easeInOutQuad (t, b, c, d) {
+        if ((t /= d / 2) < 1) return c / 2 * t * t + b;
+        return -c / 2 * ((--t) * (t - 2) - 1) + b;
+    }
+    
+    function rAf(loop) {
+        requestAnimationFrame(loop)
+    }
+    
+    const box = document.querySelector('.box')
+    
+    let boxPos = {
+        x: 0,
+        y: 0
+    }
+    let mousePos = {
+        x: 0,
+        y: 0
+    }
+    let boxScale = 1
+    
+    window.addEventListener('mousemove', (e)=> {
+        mousePos.x = e.clientX - 50
+        mousePos.y = e.clientY - 50
+    }) 
+    let incrementScale
+    
+    function start() {
+        incrementScale = setInterval(()=> {
+            boxScale = boxScale + 0.01
+        }, 20)
+    }
+    function stop (){
+        clearInterval(incrementScale)
+        boxScale = 1
+        console.log(boxScale);
+    }
+    
+    window.onmousedown = start
+    window.onmouseup = stop
+    
+    function moveBox(){
+        
+        let easing = easeInOutQuad(0, 0.1, 0.1, 1)
+    
+        boxPos.x = lerp(boxPos.x, mousePos.x, easing )
+        boxPos.y = lerp(boxPos.y, mousePos.y,easing)
+    
+        let a = mousePos.x - boxPos.x
+        let b = mousePos.y - boxPos.y
+        let distance = Math.sqrt( a*a + b*b)
+        
+        box.style.opacity = distance / 100 + 0.15
+        box.style.willChange = 'transform'
+        box.style.transform = `translate3d(
+        ${boxPos.x}px, 
+        ${boxPos.y}px, 0)
+        scale(${(distance / 700) + 0.3})`
+        
+        rAf(moveBox)
+    }
+    moveBox()
+}
+main()
+
+
+
+   
+    
+
+   
+
+
+
+
+
